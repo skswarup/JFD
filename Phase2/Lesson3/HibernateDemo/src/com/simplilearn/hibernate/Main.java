49434bd1bac272fa78bf8c716981a6c47f728e58
skswarup <skswarup30@gmail.com>
2022-03-04 21:40:44 +0530

    PracticeProject

---
+++ Phase2/Lesson3/HibernateDemo/src/com/simplilearn/hibernate/Main.java
@@ -0,0 +1,30 @@
+package com.simplilearn.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+public static void main(String[] args) {
+	StandardServiceRegistry registery= 
+			new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+	Metadata metadata= new MetadataSources(registery).getMetadataBuilder().build();
+	
+	SessionFactory factory= metadata.getSessionFactoryBuilder().build();
+	
+	Session session= factory.openSession();
+	
+	Student s1= new Student("siba","Sahoo","sibananda@gmail.com");
+	Transaction t= session.beginTransaction();
+	session.save(s1);
+	t.commit();
+	System.out.println("Data Inserted");
+	
+	session.close();
+	factory.close();
+}
+}
